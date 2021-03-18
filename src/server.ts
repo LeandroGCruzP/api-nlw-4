4090f835daaefbdcf56b431a0dea50dd32c76ab9
Leandro Geovanni Cruz <leandro.programmer@hotmail.com>
2021-03-18 07:04:00 -0300

    config database with typeorm and test routes

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,17 @@
+import "reflect-metadata";
 import express from "express";
+import "./database";
 
 const app = express();
 
+app.get("/", (request, response) => {
+  return response.json({ message: "List Ale" });
+});
+
+app.post("/", (request, response) => {
+  return response.json({ message: "Criado com susseso" });
+});
+
 app.listen(3333, () => console.log("Server is running"));
+
+// Dia 2: 18:14
